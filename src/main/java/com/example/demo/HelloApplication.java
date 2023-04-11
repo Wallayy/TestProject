352408a14148847cf96032ff4c02818dc24cca1d
Wally <wallyyopp@gmail.com>
2023-04-11 15:48:13 -0700

    Added moving message on button click.

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,11 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+    /*
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
@@ -16,8 +29,67 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-    //testtesttesttesttestsss
+     */
+
+    public void start(Stage primaryStage) {
+        System.out.println("Program started!");
+        primaryStage.setTitle("Button thingy");
+        Button btn = new Button();
+        btn.setText("This is a button!");
+
+        // create a label
+        Label label = new Label("Caution Message here!");
+        label.setAlignment(Pos.CENTER); // Center the text on the label
+
+        // create a popup
+        Popup popup = new Popup();
+
+        // set background
+        label.setStyle(" -fx-background-color: white;");
+
+        // add the label
+        popup.getContent().add(label);
+
+        // set size of label
+        label.setMinWidth(500);
+        label.setMinHeight(75);
+
+        // Make the popup draggable
+        final Delta dragDelta = new Delta();
+        label.setOnMousePressed(mouseEvent -> {
+            dragDelta.x = popup.getX() - mouseEvent.getScreenX();
+            dragDelta.y = popup.getY() - mouseEvent.getScreenY();
+        });
+
+        label.setOnMouseDragged(mouseEvent -> {
+            popup.setX(mouseEvent.getScreenX() + dragDelta.x);
+            popup.setY(mouseEvent.getScreenY() + dragDelta.y);
+        });
+
+        // action event
+        EventHandler<ActionEvent> event =
+                new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        if (!popup.isShowing())
+                            popup.show(primaryStage);
+                        else
+                            popup.hide();
+                    }
+                };
+
+        btn.setOnAction(event);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+    // Delta class to store x and y coordinates
+    private static class Delta {
+        double x, y;
+    }
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
