2f2b2b3876c684b3ed059a6d7f26f40e8c84c943
Wally <wallyyopp@gmail.com>
2023-04-11 16:00:18 -0700

    This is version 1
    Moving message on button click

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -30,7 +30,7 @@ public class HelloApplication extends Application {
         stage.show();
     }
      */
-
+    //hi
     public void start(Stage primaryStage) {
         System.out.println("Program started!");
         primaryStage.setTitle("Button thingy");
