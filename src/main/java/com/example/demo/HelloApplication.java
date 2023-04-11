791bc5e065b31a69e4e804ba6bdbcabb6cb6c5c1
Wally <wallyyopp@gmail.com>
2023-04-11 16:03:22 -0700

    test - new branch

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    /*
+
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
@@ -29,65 +29,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-     */
-    public void start(Stage primaryStage) {
-        System.out.println("Program started!");
-        primaryStage.setTitle("Button thingy");
-        Button btn = new Button();
-        btn.setText("This is a button!");
 
-        // create a label
-        Label label = new Label("Caution Message here!");
-        label.setAlignment(Pos.CENTER); // Center the text on the label
-
-        // create a popup
-        Popup popup = new Popup();
-
-        // set background
-        label.setStyle(" -fx-background-color: white;");
-
-        // add the label
-        popup.getContent().add(label);
-
-        // set size of label
-        label.setMinWidth(500);
-        label.setMinHeight(75);
-
-        // Make the popup draggable
-        final Delta dragDelta = new Delta();
-        label.setOnMousePressed(mouseEvent -> {
-            dragDelta.x = popup.getX() - mouseEvent.getScreenX();
-            dragDelta.y = popup.getY() - mouseEvent.getScreenY();
-        });
-
-        label.setOnMouseDragged(mouseEvent -> {
-            popup.setX(mouseEvent.getScreenX() + dragDelta.x);
-            popup.setY(mouseEvent.getScreenY() + dragDelta.y);
-        });
-
-        // action event
-        EventHandler<ActionEvent> event =
-                new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        if (!popup.isShowing())
-                            popup.show(primaryStage);
-                        else
-                            popup.hide();
-                    }
-                };
-
-        btn.setOnAction(event);
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
-
-    // Delta class to store x and y coordinates
-    private static class Delta {
-        double x, y;
-    }
     public static void main(String[] args) {
         launch(args);
     }
