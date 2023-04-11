d9f8728a71e5fb5f0872b4f4ae477d77b87de37b
Wally <wallyyopp@gmail.com>
2023-04-11 16:03:56 -0700

    test - new branch

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -29,7 +29,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    //
     public static void main(String[] args) {
         launch(args);
     }
